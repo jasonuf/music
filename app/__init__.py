8f7eef1bea9a3429023d2d9f52ac2ef3051c4712
tacoJason <155794000+tacoJason@users.noreply.github.com>
2025-07-07 00:55:11 -0400

    config

--- app/__init__.py
+++ app/__init__.py
@@ -3,9 +3,11 @@
 from flask import Flask
 from flask_apscheduler import APScheduler
 from config import Config
+import os
 
 app = Flask(__name__)
-app.config.from_object(Config)
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY') or os.urandom(20).hex()
+
 
 from app import routes, schedule_manager
 
